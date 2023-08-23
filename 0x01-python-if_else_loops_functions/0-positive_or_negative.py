29b42d63f7d2e21f4e0d96933a96effa84e7621a
Alex Kinyua <alexkinyua001alx@gmail.com>
2023-08-23 01:11:57 -0700

    chck if random no is + or -

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -2,7 +2,7 @@
 import random
 number = random.randint(-10, 10)
 if number > 0:
-    print(f'{number} is postive')
+    print(f'{number:d} is postive')
 elif number == 0:
     print(f'{number} is zero')
 else:
