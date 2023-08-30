dc2e938402fc133c480259102b30ae8526b5bd86
Alex Kinyua <alexkinyua001alx@gmail.com>
2023-08-30 00:09:10 -0700

    Write an empty class Square that defines a square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-
 """Define a class Square."""
 
 
 class Square:
     """Opening a class called square."""
     pass
-"""We use pass because it should have at least one line of code"""
