be62fbe44c16c8092486ea0e28278a3b07451edc
Alex Kinyua <alexkinyua001alx@gmail.com>
2023-10-18 10:22:13 -0700

    creates the database hbtn_0d_2 and the user user_0d_2.

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
--- creates the database hbtn_0d_2 and the user user_0d_2.
+-- create a user and password with select priviliges
 CREATE database IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@localhost IDENTIFIED BY 'user_0d_2_pwd';
 GRANT USAGE on *.* TO 'user_0d_2'@'localhost';
