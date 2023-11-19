57953e78857bc9fde403613a2a98fc03d82b79e0
Alex Kinyua <alexkinyua001alx@gmail.com>
2023-11-19 02:19:41 -0800

    push task 3

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" script that takes in an argument and displays
+all values in the states table of hbtn_0e_0_usa 
+where name matches the argument """
+import MySQLdb
+from sys import argv
+
+# The code should not be executed when imported
+if __name__ == '__main__':
+
+    # connecting to the database
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE BINARY name = %s", [argv[4]])
+
+    rows = cur.fetchall()
+    for i in rows:
+        print(i)
+    # Clean up
+    cur.close()
+    db.close()
+
